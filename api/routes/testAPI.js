92f13b9ff94521b6ff2b2961cff09a6796568701
raresmigea <raresini@yahoo.com>
2020-07-12 22:56:06 +0300

    established communication between api and client

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-  res.send('api working');
+  res.send('data from API');
 });
 
 module.exports = router;
