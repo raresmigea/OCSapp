9226ecf4600dae199aace2bbd14f3be73219c0d9
raresmigea <raresini@yahoo.com>
2020-07-13 00:23:21 +0300

    connect to SQLite db and send data in react

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./db/ocs_athletes.db');
 
 router.get('/', function (req, res, next) {
-  res.send('data from API');
+  db.serialize(function () {
+    db.each('SELECT athlete_id, name, surname FROM Athlete', function (
+      err,
+      row
+    ) {
+      res.send(row);
+    });
+  });
+
+  db.close();
 });
 
 module.exports = router;
