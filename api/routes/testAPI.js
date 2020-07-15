9363d2175d21b295353515432dd36a70197b699a
raresmigea <raresini@yahoo.com>
2020-07-15 23:44:06 +0300

    Combine all data in one table and send it to React

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -5,20 +5,13 @@ var db = new sqlite3.Database('./db/ocs_athletes.db');
 
 router.get('/', function (req, res, next) {
   db.serialize(function () {
-    db.all('SELECT name FROM sqlite_master WHERE type="table"', function (
-      err,
-      rows
-    ) {
-      const result = [];
-      rows.forEach(
-        (table) => awaitdb.run('SELECT * FROM ' + table.name),
-        function (err, innerRows) {
-          result.push(innerRows);
-        }
-      );
-      // console.log('restult: ', result);
-      return res.send(result);
-    });
+    db.all(
+      // 'SELECT name FROM sqlite_master WHERE type="table"',
+      'SELECT a.athlete_id, a.name, a.surname, a.date_of_birth, a.bio, a.height, a.weight, ap.photo, ap.mime_type,ar.gold, ar.silver, ar.bronze, g.city, g.year FROM(((Athlete a JOIN AthletePhoto ap ON a.photo_id = ap.photo_id) JOIN AthleteResult ar ON a.athlete_id = ar.athlete_id) JOIN Game g ON ar.game_id = g.game_id)',
+      function (err, rows) {
+        return res.send(rows);
+      }
+    );
   });
 });
 
