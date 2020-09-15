77124ff8869799b7c99f83d7878d70c44088e703
raresmigea <raresini@yahoo.com>
2020-09-15 22:54:13 +0300

    updated messages

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 var router = Router();
 
-/* GET home page. */
+// GET home page
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 var router = Router();
 
-/* GET users listing. */
+// GET users listing
 router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
